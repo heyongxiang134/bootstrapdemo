6d5ac8f15b27b8735665a4a20053f308ca75283c
‘heyongxiang134 <13420132617@163.com>
2019-04-12 19:24:56 +0800

    Initial commit

---
+++ src/main/java/com/qf/pojo/Car.java
@@ -0,0 +1,31 @@
+package com.qf.pojo;
+
+public class Car {
+    private Integer id;
+    private String name;
+    private String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
